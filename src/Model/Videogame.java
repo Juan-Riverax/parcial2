60472028362be647b438adf6527fb836997acb4d
Juan Rivera <juanjisx2@gmail.com>
2026-05-24 20:23:39 -0500

    objetillo videogame

---
+++ src/Model/Videogame.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Videogame {
+    private int id;
+    private String name;
+    private generos genero;
+    private double precio;
+    private int unidades;
+}
---
+++ src/Model/generos.java
@@ -0,0 +1,5 @@
+package Model;
+
+public enum generos {
+    Accion,Fantasia,Shooter,Terror,Plataformas,Aventura,Carreras,Peleas,Deportes,RPG,Simulacion,Estrategia,Detectives,Realidad_Aumentada,Sandbox,Dungeon_Crawler,Cartas,Puzzle;
+}
