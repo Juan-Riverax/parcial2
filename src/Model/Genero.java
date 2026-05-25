8fe9b7cd8e062af27ef0f2433e65983e6dbf7ebb
Juan Rivera <juanjisx2@gmail.com>
2026-05-24 22:01:18 -0500

    prestamo

--- src/Model/Genero.java
+++ src/Model/Genero.java
@@ -1,5 +1,5 @@
 package Model;
 
 public enum Genero {
-    Accion,Fantasia,Shooter,Terror,Plataformas,Aventura,Carreras,Peleas,Deportes,RPG,Simulacion,Estrategia,Detectives,Realidad_Aumentada,Sandbox,Dungeon_Crawler,Cartas,Puzzle;
+    Accion,Fantasia,Shooter,Terror,Plataformas,Aventura,Carreras,Pelea,Deportes,RPG,Simulacion,Estrategia,Detectives,Realidad_Aumentada,Sandbox,Dungeon_Crawler,Cartas,Puzzle,Sigilo,SciFi,Musical;
 }
---
+++ src/Model/Prestamo.java
@@ -0,0 +1,34 @@
+package Model;
+
+import java.sql.Timestamp;
+
+public class Prestamo {
+    private int id;
+    private Videogame videogame;
+    private String cliente;
+    private Timestamp fechaPrestamo;
+
+    public Prestamo (int id, String pais, String name){
+        this.id=id;
+        this.videogame=videogame;
+        this.cliente=cliente;
+        this.fechaPrestamo=fechaPrestamo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Videogame getVideogame() {
+        return videogame;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public Timestamp getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+}
